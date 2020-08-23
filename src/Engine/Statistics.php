a1727ea27fcd6fb5824eafd5b9e10aa974d2a828
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2020-08-23 10:19:14 +0000

    Apply fixes from StyleCI

--- src/Engine/Statistics.php
+++ src/Engine/Statistics.php
@@ -1,8 +1,12 @@
 <?php
 
+/*
+ * PHP Search Engine Project
+ *
+ * Copyright (C) 2020 Joshua Crocker
+ */
 
 namespace Crockerio\SearchEngine\Engine;
-
 
 use Crockerio\SearchEngine\Database\Models\Document;
 use Crockerio\SearchEngine\Database\Models\Domain;
